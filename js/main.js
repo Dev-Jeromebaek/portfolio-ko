c5561b6975142e0daff1f2e23df46164f191783f
Dev-JeromeBaek <superior051@icloud.com>
2018-10-27 15:09:05 +0900

    Docs: Update Personal activity and Resume content
    1. 포드폴리오 메인 페이지 업데이트
    2. resume 개인 활동 및 개인 프로젝트 추가
    3. 불필요한 주석 제거

--- js/main.js
+++ js/main.js
@@ -1,45 +1,49 @@
 /*global $, jQuery, alert*/
 $(document).ready(function() {
-
-  'use strict';
+  "use strict";
 
   // ========================================================================= //
   //  //SMOOTH SCROLL
   // ========================================================================= //
 
-
   $(document).on("scroll", onScroll);
 
-  $('a[href^="#"]').on('click', function(e) {
+  $('a[href^="#"]').on("click", function(e) {
     e.preventDefault();
     $(document).off("scroll");
 
-    $('a').each(function() {
-      $(this).removeClass('active');
+    $("a").each(function() {
+      $(this).removeClass("active");
       if ($(window).width() < 768) {
-        $('.nav-menu').slideUp();
+        $(".nav-menu").slideUp();
       }
     });
 
-    $(this).addClass('active');
+    $(this).addClass("active");
 
     var target = this.hash,
-        menu = target;
+      menu = target;
 
     target = $(target);
-    $('html, body').stop().animate({
-      'scrollTop': target.offset().top - 80
-    }, 500, 'swing', function() {
-      window.location.hash = target.selector;
-      $(document).on("scroll", onScroll);
-    });
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: target.offset().top - 80
+        },
+        500,
+        "swing",
+        function() {
+          window.location.hash = target.selector;
+          $(document).on("scroll", onScroll);
+        }
+      );
   });
 
-
   function onScroll(event) {
-    if ($('.home').length) {
+    if ($(".home").length) {
       var scrollPos = $(document).scrollTop();
-      $('nav ul li a').each(function() {
+      $("nav ul li a").each(function() {
         var currLink = $(this);
         var refElement = $(currLink.attr("href"));
       });
@@ -50,16 +54,15 @@ $(document).ready(function() {
   //  //NAVBAR SHOW - HIDE
   // ========================================================================= //
 
-
   $(window).scroll(function() {
     var scroll = $(window).scrollTop();
-    if (scroll > 200 ) {
+    if (scroll > 200) {
       $("#main-nav, #main-nav-subpage").slideDown(700);
-      $("#main-nav-subpage").removeClass('subpage-nav');
+      $("#main-nav-subpage").removeClass("subpage-nav");
     } else {
       $("#main-nav").slideUp(700);
       $("#main-nav-subpage").hide();
-      $("#main-nav-subpage").addClass('subpage-nav');
+      $("#main-nav-subpage").addClass("subpage-nav");
     }
   });
 
@@ -67,8 +70,8 @@ $(document).ready(function() {
   //  // RESPONSIVE MENU
   // ========================================================================= //
 
-  $('.responsive').on('click', function(e) {
-    $('.nav-menu').slideToggle();
+  $(".responsive").on("click", function(e) {
+    $(".nav-menu").slideToggle();
   });
 
   // ========================================================================= //
@@ -81,54 +84,49 @@ $(document).ready(function() {
     typed.typed({
       strings: ["Hi, I'm Jerome Baek.", "I'm Web Developer."],
       typeSpeed: 100,
-      loop: true,
+      loop: true
     });
   });
-
 
   // ========================================================================= //
   //  Owl Carousel Services
   // ========================================================================= //
 
-
-  $('.services-carousel').owlCarousel({
-      autoplay: true,
-      loop: true,
-      margin: 20,
-      dots: true,
-      nav: false,
-      responsiveClass: true,
-      responsive: { 0: { items: 1 }, 768: { items: 2 }, 900: { items: 4 } }
-    });
-
+  $(".services-carousel").owlCarousel({
+    autoplay: true,
+    loop: true,
+    margin: 20,
+    dots: true,
+    nav: false,
+    responsiveClass: true,
+    responsive: { 0: { items: 1 }, 768: { items: 2 }, 900: { items: 4 } }
+  });
 
   // ========================================================================= //
   //  Porfolio isotope and filter
   // ========================================================================= //
 
-
-  var portfolioIsotope = $('.portfolio-container').isotope({
-    itemSelector: '.portfolio-thumbnail',
-    layoutMode: 'fitRows'
+  var portfolioIsotope = $(".portfolio-container").isotope({
+    itemSelector: ".portfolio-thumbnail",
+    layoutMode: "fitRows"
   });
 
-  $('#portfolio-flters li').on( 'click', function() {
-    $("#portfolio-flters li").removeClass('filter-active');
-    $(this).addClass('filter-active');
+  $("#portfolio-flters li").on("click", function() {
+    $("#portfolio-flters li").removeClass("filter-active");
+    $(this).addClass("filter-active");
 
-    portfolioIsotope.isotope({ filter: $(this).data('filter') });
+    portfolioIsotope.isotope({ filter: $(this).data("filter") });
   });
-
 
   // ========================================================================= //
   //  magnificPopup
   // ========================================================================= //
 
   var magnifPopup = function() {
-    $('.popup-img').magnificPopup({
-      type: 'image',
+    $(".popup-img").magnificPopup({
+      type: "image",
       removalDelay: 300,
-      mainClass: 'mfp-with-zoom',
+      mainClass: "mfp-with-zoom",
       gallery: {
         enabled: true
       },
@@ -136,7 +134,7 @@ $(document).ready(function() {
         enabled: true, // By default it's false, so don't forget to enable it
 
         duration: 300, // duration of the effect, in milliseconds
-        easing: 'ease-in-out', // CSS transition easing function
+        easing: "ease-in-out", // CSS transition easing function
 
         // The "opener" function should return the element from which popup will be zoomed in
         // and to which popup will be scaled down
@@ -144,25 +142,11 @@ $(document).ready(function() {
         opener: function(openerElement) {
           // openerElement is the element on which popup was initialized, in this case its <a> tag
           // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-          return openerElement.is('img') ? openerElement : openerElement.find('img');
+          return openerElement.is("img")
+            ? openerElement
+            : openerElement.find("img");
         }
       }
-    });
-  };
-
-  // ========================================================================= //
-  //  pieChart
-  // ========================================================================= //
-
-  var pieChart = function() {
-    $('.chart').easyPieChart({
-      scaleColor: false,
-      lineWidth: 4,
-      lineCap: 'butt',
-      barColor: '#FF9000',
-      trackColor: "#f5f5f5",
-      size: 160,
-      animate: 1000
     });
   };
 
@@ -171,7 +155,7 @@ $(document).ready(function() {
   // ========================================================================= //
 
   var circle_chart_event = function() {
-    var c = document.getElementById('cir-next-btn');
+    var c = document.getElementById("cir-next-btn");
     c.click();
   };
 
@@ -191,16 +175,20 @@ $(document).ready(function() {
     $("#locale_check").on("change", function() {
       var language = $("#locale_check").val();
       console.log(language);
-      if(language === "ko") {
-        var ko_sel_check = $("#locale_check").children()[0].getAttribute("selected");
-        if(ko_sel_check === null) {
-          location.href="http://jeromebaek-portfolio-ko.ml";
+      if (language === "ko") {
+        var ko_sel_check = $("#locale_check")
+          .children()[0]
+          .getAttribute("selected");
+        if (ko_sel_check === null) {
+          location.href = "http://jeromebaek-portfolio-ko.ml";
         }
       } else {
-        var en_sel_check = $("#locale_check").children()[1].getAttribute("selected");
+        var en_sel_check = $("#locale_check")
+          .children()[1]
+          .getAttribute("selected");
         console.log(en_sel_check);
-        if(en_sel_check === null) {
-          location.href="http://jeromebaek-portfolio-en.ml";
+        if (en_sel_check === null) {
+          location.href = "http://jeromebaek-portfolio-en.ml";
         }
       }
     });
@@ -210,10 +198,10 @@ $(document).ready(function() {
   //  이력서 더보기 버튼 이벤트
   // ========================================================================= //
   var moreResume = function() {
-     $("#moreResume").on("click", function() {
-        location.href="portfolio/index.html";
+    $("#moreResume").on("click", function() {
+      location.href = "portfolio/index.html";
     });
-  }
+  };
 
   // Call the functions
   magnifPopup();
@@ -222,6 +210,6 @@ $(document).ready(function() {
   locale_check();
   moreResume();
   setInterval(function() {
-    document.getElementById('cir-next-btn').click();
+    document.getElementById("cir-next-btn").click();
   }, 4000);
 });
--- portfolio/dist/bundle.js
+++ portfolio/dist/bundle.js
@@ -9259,11 +9259,11 @@ exports.push([module.i, "/* reset css */\n/* http://meyerweb.com/eric/tools/css/
 
 module.exports = {
   profile: {
-    name: '백승엽',
-    description: 'Web developer',
-    dateOfBirth: '1991.04.30',
-    email: 'superior051@icloud.com',
-    links: [{ name: 'portfolio', href: 'https://jeromeBaek-portfolio.ml' }, { name: 'github', href: 'https://github.com/Dev-JeromeBaek' }, { name: 'pinterest', href: 'https://www.pinterest.co.kr/jeromebaek/' }, { name: 'blog', href: 'https://blog.naver.com/backsajang420' }]
+    name: "백승엽",
+    description: "Web developer",
+    dateOfBirth: "1991.04.30",
+    email: "superior051@icloud.com",
+    links: [{ name: "portfolio", href: "https://jeromeBaek-portfolio.ml" }, { name: "github", href: "https://github.com/Dev-JeromeBaek" }, { name: "pinterest", href: "https://www.pinterest.co.kr/jeromebaek/" }, { name: "blog", href: "https://blog.naver.com/backsajang420" }]
   },
   introduction: `
     깨끗하고 단순하며 실용적인 웹 사이트 만들기를 추구하고 좋아하는 개발자 백승엽 입니다.<br />
@@ -9274,155 +9274,167 @@ module.exports = {
     그 외 각종 커뮤니티를 통해 icon, template, UI kit, Pattern을 수집하기도 하고, blog, github, pinterest 를 통해 다양한 개발 지식과 정보를 공유, 관리하고있습니다.<br />
     `,
   contents: {
-    'Skill Set': [{
-      date: '(상급: 3, 중급: 2, 초급: 1)',
-      title: '',
-      contents: ['']
+    "Skill Set": [{
+      date: "(상급: 3, 중급: 2, 초급: 1)",
+      title: "",
+      contents: [""]
     }, {
-      date: 'Language',
-      title: 'JavaScript (3) / HTML5 (3) / CSS3 (3) / JAVA (3) / JSP(3) / TypeScript (1) / PHP (2) ',
-      contents: ['']
+      date: "Language",
+      title: "JavaScript (3) / JAVA (3) / JSP(3) / TypeScript (1) / PHP (2) ",
+      contents: [""]
     }, {
-      date: 'Framework',
-      title: 'Spring (2) / Bootstrap (2) / Codeigniter (2) / _Template (2)',
-      contents: ['']
+      date: "Framework",
+      title: "Spring (2) / React (2) / Codeigniter (2) / _Template (2)",
+      contents: [""]
     }, {
-      date: 'Library',
-      title: ' React (2) / JSTL (3) / jQuery (2)',
-      contents: ['']
+      date: "Library",
+      title: "Redux (1) / MobX(1) / JSTL (3) / jQuery (2)",
+      contents: [""]
     }, {
-      date: 'Dependency Manager',
-      title: 'npm (2) / WebPack (2) / BaBel (2) / Maven(2)',
-      contents: ['']
+      date: "Markup",
+      title: "HTML5 (3) / CSS3 (3) / Sass(1) / Bootstrap (2) / Meterial-design (1)",
+      contents: [""]
     }, {
-      date: 'Data Base',
-      title: 'Mysql (3) / Oracle (2) / OrientDB (1)',
-      contents: ['']
+      date: "Dependency Manager",
+      title: "npm (2) / yarn (2) / WebPack (2) / BaBel (2) / Maven(2)",
+      contents: [""]
+    }, {
+      date: "Data Base",
+      title: "Mysql (3) / Oracle (2) / OrientDB (1)",
+      contents: [""]
     }],
 
-    // '개인 프로젝트': [{
-    //   'date': '2017.07 - 현재',
-    //   'title': 'vue-lunar-calendar',
-    //   'contents': ['- Vue.js component library', '- <a href="https://github.com/KimWooHyun/vue-lunar-calendar">https://github.com/KimWooHyun/vue-lunar-calendar</a>', '- 한국 음력 달력 라이브러리', '- opensource', '- 총 7명의 contributors, 6개의 언어']
-    // }, {
-    //   'date': '2017.08',
-    //   'title': 'WHLunarCalendar',
-    //   'contents': ['- Swift library', '- <a href="https://github.com/KimWooHyun/WHLunarCalendar">https://github.com/KimWooHyun/WHLunarCalendar</a>', '- 한국 음력 달력 라이브러리', '- opensource']
-    // }],
-
-    '개인 활동': [{
-      date: '2018.06',
-      title: '2018 오픈소스 개발자 이야기',
-      contents: ['Microsoft [meetup]', '- 오픈소스개발자로서의 경험담 공유', '<a href="https://www.slideshare.net/inureyes/ss-104038275" target="_blank">- 회색지대: 이상과 현실 - 오픈소스 저작권(Lablup Inc./신정규님)</a>', '<a href="https://s3.ap-northeast-2.amazonaws.com/kr.elastic.co/docs/jongmin-kim-OSS-Dev-Forum.pdf" target="_blank">- Elastic에서 Remote로 일하기 (Elastic/김종민님)</a>', '<a href="https://lookaside.fbsbx.com/file/opensource-seminar-20180630.pdf?token=AWxNMD6qC0hgrZrZmQqugY3DM-6tJVQlZCC6aw7wYf-mN46sdvxPSVknRsdwTMPsakXdwMjt8NyV1HhAkx3kcOJNV-lhg2zxX3Cazhtq3_cBrKVKImjcvXxlWlXMk1a15zyCl_Q7L2nPYP4RLDRI1wfONn5mQMnAgilDWgqTBR2OfQ" target="_blank">- 오픈소스 생태계 일원으로서의 개발자 (BlockchainOS SRE/변정훈님)</a>', '<a href="https://docs.google.com/presentation/d/10Ab4gAGo7n3ip7q6PbXANT2W_vKabvUUnVUODqR0WN4/mobilepresent?slide=id.p" target="_blank">- 해외 오픈소스 컨퍼런스 발표와 참여 (KossLab/송태웅님)</a>', '<a href="https://www.slideshare.net/scarinet/python-pycon-and-the-psf-103838502" target="_blank">- 파이썬, 파이콘, 파이썬소프트웨어재단 (파이썬 소프트웨어 재단/김영근님)</a>', '<a href="https://lookaside.fbsbx.com/file/%EC%9D%B4%EB%AC%B8%EC%88%98_%EB%B0%9C%ED%91%9C%EC%9E%90%EB%A3%8C.pdf?token=AWwaEYKba0QIJYo_YCqmB2xt38rNifm2iDlZXGktfDldP26RbCcqz4Rg5hFgi9b5Wv2JcqGiYk_ADfh7RMHIrJi8uLII4A7quk2zJwXt8Cek09HxsDvjkMKc5v25uYoV9cuGu9o9bmABjzSIdpGgZbGTEXg0FzBBxhhhsteTfLNP5g" target="_blank">- 아파치 제플린, 프로젝트 시작부터 아파치 탑레벨 프로젝트가 되기까지(ZELP/이문수님)</a>']
+    "개인 프로젝트": [{
+      date: "2018.10 - 현재",
+      title: "awesome-web-styling",
+      contents: ["- Pure CSS Animation Effects", '- <a href="https://github.com/Dev-JeromeBaek/awesome-web-styling">https://github.com/Dev-JeromeBaek/awesome-web-styling</a>', "- 웹페이지에 사용하는 각각 부품들에 대한 Animation Effect 제공", "- Navigation, Text, Card, Spinner, Hamburger, etc", "- opensource"]
     }, {
-      date: '2018.06',
-      title: 'Google Cloud OnBoard - Seoul',
-      contents: ['Google Cloud [education]', '- 세션별 기술 시연', '- Google App Engine, Datastore, Storage, Container Engine, Computer Engine 및 네트워크, 빅데이터, 기계 학습', '- 기술교육.', '(수료증 보유)']
-    }, {
-      date: '2018.06',
-      title: 'Designer & Developer',
-      contents: ['GDG Campus Korea [meetup]', '- 디자이너 & 개발자 협업', '- 실무진 이야기', '- 네트워킹.']
-    }, {
-      date: '2018.02',
-      title: 'HTML, CSS, and NEXT?',
-      contents: ['CodeSquad [seminar]', '- front-end 개발에 대한 전반적인 방향성과 현재 이슈를 포함한 세미나', '- HTML5, CSS3에 대한 심화 과정 스터디 진행.']
-    }, {
-      date: '2017.11 - 2017.12',
-      title: 'Type Script Basic',
-      contents: ['TypeScript Korea [seminar]', '- 타입스크립트 코리아 유저그룹 기초 세미나 참여', '- 타입스크립트 기초 문법 학습.']
-    }, {
-      date: '2017.07 - 2017.08',
-      title: 'Code-Night 2nd',
-      contents: ['Opentutorials [education]', '- 웹 어플리케이션 개발 기초 및 이슈사항 등을 포함한 웹 개발의 전반적인 내용 스터디', '- Google, YouTube, TPay, Opentutorials 개발자가 함께 지원하여 진행', '-2기 졸업.', '(수료증 보유)']
-    }, {
-      date: '2015.10 - 2017.03',
-      title: 'Ready for Developer',
-      contents: ['IT Bank [education]', '- Java, JSP, Spring, Network, Linux, Window Server를 단과 교육 과정을 통해 수료하였으며, 마지막으로 4개월의 웹 개발 과정 취업반 수료.', '(수료증 보유)']
+      date: "2018.08",
+      title: "awesome-react-skeleton",
+      contents: ["- React, Redux library", '- <a href="https://github.com/Dev-JeromeBaek/awesome-react-basic-skeleton">https://github.com/Dev-JeromeBaek/awesome-react-basic-skeleton</a>', "- React 공부를 위한 시작 skeleton 프로젝트 제공", "- opensource"]
     }],
 
-    '학력 사항': [{
-      date: '2010 - 2016',
-      title: '중부대학교',
-      contents: ['정보통신학과 졸업']
+    "개인 활동": [{
+      date: "2018.11",
+      title: "GDG DevFest Seoul 2018",
+      contents: ["GDG Tech [Festival]", "- 구글 기술과 관련된 세션, 코드랩, 스터디잼 구성", "- Android, Firebase, GCP, Machine Learning, Web Tech, Chrome 등 기술 소개", "- 기술 트랜드 공유"]
     }, {
-      date: '2012 - 2015',
-      title: '서울성남고등학교',
-      contents: ['인문계 (이공계) 졸업']
+      date: "2018.10",
+      title: "뻔뻔한 발표",
+      contents: ["Fun Fun Study", "- 함수형 프로그래밍이란?", "- 코틀린, ARCore, Tensorflow에서의 함수형 프로그래밍 짧은 이론 및 경험 공유"]
+    }, {
+      date: "2018.06",
+      title: "2018 오픈소스 개발자 이야기",
+      contents: ["Microsoft [meetup]", "- 오픈소스개발자로서의 경험담 공유", '<a href="https://www.slideshare.net/inureyes/ss-104038275" target="_blank">- 회색지대: 이상과 현실 - 오픈소스 저작권(Lablup Inc./신정규님)</a>', '<a href="https://s3.ap-northeast-2.amazonaws.com/kr.elastic.co/docs/jongmin-kim-OSS-Dev-Forum.pdf" target="_blank">- Elastic에서 Remote로 일하기 (Elastic/김종민님)</a>', '<a href="https://lookaside.fbsbx.com/file/opensource-seminar-20180630.pdf?token=AWxNMD6qC0hgrZrZmQqugY3DM-6tJVQlZCC6aw7wYf-mN46sdvxPSVknRsdwTMPsakXdwMjt8NyV1HhAkx3kcOJNV-lhg2zxX3Cazhtq3_cBrKVKImjcvXxlWlXMk1a15zyCl_Q7L2nPYP4RLDRI1wfONn5mQMnAgilDWgqTBR2OfQ" target="_blank">- 오픈소스 생태계 일원으로서의 개발자 (BlockchainOS SRE/변정훈님)</a>', '<a href="https://docs.google.com/presentation/d/10Ab4gAGo7n3ip7q6PbXANT2W_vKabvUUnVUODqR0WN4/mobilepresent?slide=id.p" target="_blank">- 해외 오픈소스 컨퍼런스 발표와 참여 (KossLab/송태웅님)</a>', '<a href="https://www.slideshare.net/scarinet/python-pycon-and-the-psf-103838502" target="_blank">- 파이썬, 파이콘, 파이썬소프트웨어재단 (파이썬 소프트웨어 재단/김영근님)</a>', '<a href="https://lookaside.fbsbx.com/file/%EC%9D%B4%EB%AC%B8%EC%88%98_%EB%B0%9C%ED%91%9C%EC%9E%90%EB%A3%8C.pdf?token=AWwaEYKba0QIJYo_YCqmB2xt38rNifm2iDlZXGktfDldP26RbCcqz4Rg5hFgi9b5Wv2JcqGiYk_ADfh7RMHIrJi8uLII4A7quk2zJwXt8Cek09HxsDvjkMKc5v25uYoV9cuGu9o9bmABjzSIdpGgZbGTEXg0FzBBxhhhsteTfLNP5g" target="_blank">- 아파치 제플린, 프로젝트 시작부터 아파치 탑레벨 프로젝트가 되기까지(ZELP/이문수님)</a>']
+    }, {
+      date: "2018.06",
+      title: "Google Cloud OnBoard - Seoul",
+      contents: ["Google Cloud [education]", "- 세션별 기술 시연", "- Google App Engine, Datastore, Storage, Container Engine, Computer Engine 및 네트워크, 빅데이터, 기계 학습", "- 기술교육.", "(수료증 보유)"]
+    }, {
+      date: "2018.06",
+      title: "Designer & Developer",
+      contents: ["GDG Campus Korea [meetup]", "- 디자이너 & 개발자 협업", "- 실무진 이야기", "- 네트워킹."]
+    }, {
+      date: "2018.02",
+      title: "HTML, CSS, and NEXT?",
+      contents: ["CodeSquad [seminar]", "- front-end 개발에 대한 전반적인 방향성과 현재 이슈를 포함한 세미나", "- HTML5, CSS3에 대한 심화 과정 스터디 진행."]
+    }, {
+      date: "2017.11 - 2017.12",
+      title: "Type Script Basic",
+      contents: ["TypeScript Korea [seminar]", "- 타입스크립트 코리아 유저그룹 기초 세미나 참여", "- 타입스크립트 기초 문법 학습."]
+    }, {
+      date: "2017.07 - 2017.08",
+      title: "Code-Night 2nd",
+      contents: ["Opentutorials [education]", "- 웹 어플리케이션 개발 기초 및 이슈사항 등을 포함한 웹 개발의 전반적인 내용 스터디", "- Google, YouTube, TPay, Opentutorials 개발자가 함께 지원하여 진행", "-2기 졸업.", "(수료증 보유)"]
+    }, {
+      date: "2015.10 - 2017.03",
+      title: "Ready for Developer",
+      contents: ["IT Bank [education]", "- Java, JSP, Spring, Network, Linux, Window Server를 단과 교육 과정을 통해 수료하였으며, 마지막으로 4개월의 웹 개발 과정 취업반 수료.", "(수료증 보유)"]
+    }],
+
+    "학력 사항": [{
+      date: "2010 - 2016",
+      title: "중부대학교",
+      contents: ["정보통신학과 졸업"]
+    }, {
+      date: "2012 - 2015",
+      title: "서울성남고등학교",
+      contents: ["인문계 (이공계) 졸업"]
     }]
   },
   summary: {
-    '경력 요약': [{
-      date: ['2018.07', '2018.08'],
-      title: 'TMON (주)티몬 - 인턴',
-      contents: ['주 - Front End 개발 (React)', '서브 - Back End 개발 (JSP/SPRING)']
+    "경력 요약": [{
+      date: ["2018.07", "2018.08"],
+      title: "TMON (주)티몬 - 인턴",
+      contents: ["주 - Front End 개발 (React)", "서브 - Back End 개발 (JSP/SPRING)"]
     }, {
-      date: ['2017.05', '2018.06'],
-      title: 'Coreriver (주)코아리버',
-      contents: ['주 - Web 개발', '서브 - Hybrid App 개발']
+      date: ["2017.05", "2018.06"],
+      title: "Coreriver (주)코아리버",
+      contents: ["주 - Web 개발", "서브 - Hybrid App 개발"]
     }]
   },
   detailContents: {
-    'TMON (티몬) - 인턴': {
-      date: '2018.07 - 2018.08',
-      info: '사내 시스템 대시보드 개발',
-      service: ['Project<br /> - <a onClick="alert(`업데이트중입니다.`)" style="text-decoration:underline;cursor:pointer">01. TMON DashBoard <a/>'],
+    "TMON (티몬) - 인턴": {
+      date: "2018.07 - 2018.08",
+      info: "국내 최초의 소셜 커머스 회사로서 중견기업, 2017년 매출 4,000억, 종업원수 1000여명, 솔루션 / 서비스 / 배송 / 영상제작 등의 업무 영역을 가지고 있음.",
+      service: ['Project<br /> - <a onClick="alert(`업체의 공개 제한요청으로 비공개처리 되었습니다.`)" style="text-decoration:underline;cursor:pointer">01. TMON DashBoard <a/>'],
       project: {
         project: [{
-          date: '2018.07 - 2018.08',
-          title: 'TMON DashBoard',
-          contents: ['사내 시스템 대시보드 개발', `
+          date: "2018.07 - 2018.08",
+          title: "TMON DashBoard",
+          contents: ["사내 시스템 대시보드 개발", `
               <br />
               <b>- [프로젝트 작업]</b> : 전체적인 제안, 기획, 설계 / Framework설계 / 프로젝트 전체 통합 / 일정관리<br /><br />
               <b>- [메인 작업]</b> : React로 SPA, Admin API List 연결 CRUD, DashBoard Service Page (Component Set, Routing, Chart, CRUD, Exception, Set LifeCycle, LocalStorage Management, Responsive Web, ...), git(Code review)<br /><br />
-              `, '- 사내 관리를 위한 관리자페이지 및 대시보드 개발', '- 기여도 : 40%', '- stack : JAVA, JSP, Spring 4.x, React, Babel, Webpack, Chartist, Axios, ReactStrap, React-Grid-Layout, React-Context-API, Git, Bitbucket, Confluence<br />']
+              `, "- 사내 시스템 관리를 위한 관리자페이지 및 대시보드 개발", "- 기여도 : 40%", "- stack : JAVA, JSP, Spring 4.x, React, Babel, Webpack, Chartist, Axios, ReactStrap, React-Grid-Layout, React-Context-API, Git, Bitbucket, Confluence<br />"]
         }]
       }
     },
-    'Coreriver (코아리버)': {
-      date: '2017.05 - 2018.06',
-      info: '코아리버는 IoT, 영상처리, 쇼핑몰 관련 웹/앱 개발의 선도적인 기업입니다. 안드로이드, iOS, 하이브리드 웹/앱 개발 및 디자인까지 토탈 솔루션을 제공합니다.',
+    "Coreriver (코아리버)": {
+      date: "2017.05 - 2018.06",
+      info: "코아리버는 IoT, 영상처리, 쇼핑몰 관련 웹/앱 개발의 선도적인 기업입니다. 안드로이드, iOS, 하이브리드 웹/앱 개발 및 디자인까지 토탈 솔루션을 제공.",
       service: ['Web - 회사 소개 사이트<br /> - <a href="http://www.coreriver.com">http://www.coreriver.com</a> <br /><br />', 'Portfolio<br /> - <a href="http://www.coreriver.com/technology.asp#portfolio">http://www.coreriver.com/technology.asp#portfolio</a> <br /><br />', 'Project<br /> - <a href="http://jeromebaek-portfolio-en.ml/portfolio/proj04_obelab.html">03. OBELAB ( 생체신호 관리 프로그램 )<a/><br /> - <a href="http://jeromebaek-portfolio-en.ml/portfolio/proj03_bmap.html">02. 바나나맵 ( 실시간 상점 정보 어플리케이션 )<a/><br /> - <a href="http://jeromebaek-portfolio-en.ml/portfolio/proj02_vpet.html">01. 바닐라펫 ( 반려동물 분양 정보 어플리케이션 )<a/>'],
       project: {
         project: [{
-          date: '2018.01 - 2018.04',
-          title: 'OBELAB (Nirsit)',
-          contents: ['생체신호 관리 프로그램에대한 관리자 웹 페이지 개발', `
+          date: "2018.01 - 2018.04",
+          title: "OBELAB (Nirsit)",
+          contents: ["생체신호 관리 프로그램에대한 관리자 웹 페이지 개발", `
               <br />
               <b>- [프로젝트 작업]</b> : 전체적인 설계 / DB설계 / 웹 Api 및 App 연동 Api 제작 / Api 문서화 / Bootstrap 적용 / DatePicker / Firebase push / Google Push / swiper / jQuery(range, eventDrag, touchSlide) Api Custom<br /><br />
               <b>- [메인 작업]</b> : 전체적인 View 구성 및 제작 / UI퍼블리싱 / 기본 sql 구성 / 응용 sql 구성 / 상단메뉴기능 통합 모듈 제작 / path 모듈 제작 / dateTime 모듈 제작 / 전체 테이블 이벤트 통합 모듈 제작 / restful 구조 작성 / socket push 알림 기능 제작 / Spring-Security 보안처리 / 메일전송 / 비밀번호 암호화 / 필터처리 / 페이징처리 / 프로젝트 버전관리 / 다국어 기능처리 / 프로젝트 정리<br /><br />
-              `, '- 관리자 웹 페이지 개발', '- 기여도 : 90%', '- stack : JAVA, JSP, Spring 4.x, MyBatis, MySQL, Apache Tomcat 8.0, AWS, Javascript, jQuery, HTML5, CSS3, Bootstrap, Ajax, Spring-Security, Spring-Messaging, Jackson-databind, Commons-dbcp<br />']
+              `, "- 관리자 웹 페이지 개발", "- 기여도 : 90%", "- stack : JAVA, JSP, Spring 4.x, MyBatis, MySQL, Apache Tomcat 8.0, AWS, Javascript, jQuery, HTML5, CSS3, Bootstrap, Ajax, Spring-Security, Spring-Messaging, Jackson-databind, Commons-dbcp<br />"]
         }, {
-          date: '2017.08 - 2018.01',
-          title: '바나나맵',
-          contents: ['전세계 모든 상점의 실시간 영업정보를 제공받고 확인할 수 있는 앱', `
+          date: "2017.08 - 2018.01",
+          title: "바나나맵",
+          contents: ["전세계 모든 상점의 실시간 영업정보를 제공받고 확인할 수 있는 앱", `
               <br />
               <b>- [프로젝트 작업]</b> : 전체의 지도(교통, 길찾기) / 메일전송 모듈제작 / 문자 전송모듈 제작 / GoogleMap ( DistanceMatrix, geocoding, marker, cluster ) / bootstrap / swiper / jQuery(range, eventDrag, touchSlider) / excel up-down / TMAP / MAPSME / SYGIC 등 api를 custom<br /><br />
               <b>- [메인 작업]</b> : (Tmap, Google, MAPSME, SYGIC)의 navigation 연동 / 길 찾기 분기처리 / 거리-시간 계산 처리 / 일정 계산 / 관리자 Excel(up-down) 구현 / GPS(내 위치 기반 정보) 확인 / 데이터 연동.<br /><br />
-              `, '- Cordova를 이용한 Hybrid App개발', '- 관리자 웹 페이지 개발', '- 기여도 : 50%', '- stack : PHP, Codeigniter, _Template, HTML5, CSS3, JavaScript, jQuery, Ajax, Apache HTTP server v2.4, MySQL, BootStrap, GoogleMap, Tmap, MAPSME, SYGIC<br />']
+              `, "- Cordova를 이용한 Hybrid App개발", "- 관리자 웹 페이지 개발", "- 기여도 : 50%", "- stack : PHP, Codeigniter, _Template, HTML5, CSS3, JavaScript, jQuery, Ajax, Apache HTTP server v2.4, MySQL, BootStrap, GoogleMap, Tmap, MAPSME, SYGIC<br />"]
         }, {
-          date: '2017.05 - 2017.07',
-          title: '바닐라펫',
-          contents: ['반려동물 분양을 보다 편리하게 제공할 수 있는 커뮤니티 앱 (Hybrid App)', `
+          date: "2017.05 - 2017.07",
+          title: "바닐라펫",
+          contents: ["반려동물 분양을 보다 편리하게 제공할 수 있는 커뮤니티 앱 (Hybrid App)", `
               <br />
               <b>- [프로젝트 작업]</b> : 전체적인 설계 / Framework설계 / DB설계 / Hybrid App Design / Use Api[ GoogleMap, Cropie, Material Design, Bootstrap, swiper, jQuery(range, eventDrag, touchSlider)<br /><br />
               <b>- [메인 작업]</b> : 전체적인 View 구성 및 제작 / 기본 CRUD를 구성 / Image(crop, slide, subject edit, delete ) / login / logout / 메일전송 / 비밀번호 암호화 / 회원가입 / SNS로그인 / 필터처리 / 검색슬라이드 / 즐겨찾기 목록화 / 최근본 게시글 / 게시글 신고 / 좋아요기능 / 즐겨찾기 기능 / 최근본 목록 기능 / 문자전송 / 전화걸기 / 내분양글목록 / 분양글등록 / 수정 / 내정보 수정 / push알림.<br /><br />
-              `, '- Cordova를 이용한 Hybrid App개발', '- 기여도 : 100%, 단독진행', '- stack : PHP, Codeigniter, _Template, HTML5, CSS3, JavaScript, jQuery, Ajax, Apache HTTP server v2.4, MySQL, AWS, BootStrap, GoogleMap, ...<br />']
+              `, "- Cordova를 이용한 Hybrid App개발", "- 기여도 : 100%, 단독진행", "- stack : PHP, Codeigniter, _Template, HTML5, CSS3, JavaScript, jQuery, Ajax, Apache HTTP server v2.4, MySQL, AWS, BootStrap, GoogleMap, ...<br />"]
         }]
       }
     },
-    'IT Bank (아이티뱅크)': {
-      date: '2015.09 - 2016.05 / 2016.09 - 2017.04',
-      info: '개발자 준비 교육 아카데미.',
+    "IT Bank (아이티뱅크)": {
+      date: "2015.09 - 2016.05 / 2016.09 - 2017.04",
+      info: "개발자 준비 교육 아카데미.",
       service: ['Project<br /> - <a href="http://jeromebaek-portfolio-en.ml/portfolio/proj01_dasam.html">01. DASAM ( 소셜커머스 )<a/>'],
       project: {
         project: [{
-          date: '2017.03 - 2017.03',
-          title: 'DASAM(소셜커머스)',
-          contents: ['티몬을 모티브로한 소셜커머스 웹 서비스 제작', `
+          date: "2017.03 - 2017.03",
+          title: "DASAM(소셜커머스)",
+          contents: ["티몬을 모티브로한 소셜커머스 웹 서비스 제작", `
               <br />
               <b>- [프로젝트 작업]</b> : 전체적인 제안, 기획, 설계 / Framework설계 / DB설계 / 프로젝트 전체 통합 / 팀 일정관리<br /><br />
               <b>- [메인 작업]</b> : 스크롤페이징 / 롤링카운터 / 상단 메뉴 제작 / 상품 메뉴 제작 / 인기상품 top10 / Other's deal 관리 / 기업회원 개인페이지 / 관리자 페이지 / 일반회원 접속여부 확인 / 등급관리 기능 / 테이블 개수 제한 더보기-접어두기 페이징(스크롤) / 일반회원 통계 관리 페이지 구현 / 기업회원 통계 관리 페이지 구현 / 인기기업, 판매 딜 많은 기업, 위험 기업 순위 적용 / 등급에 따른 할인 및 매출 계산 구현 / 구매고객 리스트 / 딜 수량 수정기능 / 딜 삭제 기능 / 기업정보 수정 기능 / 기업정보 삭제 기능 / 매출 등급 / 기업 등급상태 기능 제작.<br /><br />
-              `, '- Spring 첫 프로젝트 제작', '- 기여도 : 70%', '- stack : JAVA, JSP, Spring 3.x, Ajax, jQuery, MVC2, Maven, MyBatis, Oracle 11g, Apache Tomcat 8.0, individual Module<br />']
+              `, "- Spring 첫 프로젝트 제작", "- 기여도 : 70%", "- stack : JAVA, JSP, Spring 3.x, Ajax, jQuery, MVC2, Maven, MyBatis, Oracle 11g, Apache Tomcat 8.0, individual Module<br />"]
         }]
       }
     }
